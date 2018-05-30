605c56f2ac9e0547c5de5be6030551434b24194c
Orzzet <adrian.lc92@gmail.com>
2018-05-30 14:41:24 +0200

    Add files via upload

--- core/utils.php
+++ core/utils.php
@@ -5,6 +5,14 @@ function arrayToJS(array $valores, string $nombreVariableJS){
     $valoresJson = json_encode($valores);
 
     echo "<script>";
-    echo 'var ' . $nombreVariableJS . ' = JSON.parse("' . $valoresJson . '")';
+    echo "var " . $nombreVariableJS . " = JSON.parse('" . $valoresJson . "')";
     echo "</script>";
+}
+
+function fromTimeToDate(int $time){
+    return date("Y-m-d", $time);
+}
+
+function fromTimeToDatetime(int $time){
+    return date("Y-m-d H:i:s", $time);
 }
