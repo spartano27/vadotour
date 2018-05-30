4773f2cfa42a085fd483e04c5fe175c1febc2224
27max <38960105+27max@users.noreply.github.com>
2018-05-30 12:07:11 +0200

    Add files via upload

--- modelo/modeloReparacion.php
+++ modelo/modeloReparacion.php
@@ -1,1 +1,81 @@
 <?php
+
+include_once 'modeloVehiculo.php';
+
+function getReparacionesCliente(PDO $con, array $id_veh_cliente,int $estado){
+	//devuelve todas las reparaciones de todos los vehiculos
+	//devuelve un array de arrays donde $reparaciones[idveh][filareparacion][columnareparacion]
+
+
+	if($estado==0){		
+	    $cont = 0;
+	    $reparaciones=[];
+	    foreach ($id_veh_cliente as $id){
+	        $reparaciones[$cont] = getReparacion($con, $id[0]);
+	        $cont++;
+	    }
+	}else{
+		$cont = 0;
+	    $reparaciones=[];
+	    foreach ($id_veh_cliente as $id){
+	        $reparaciones[$cont] = getReparacion($con, $id[0],$estado);
+	        $cont++;
+	}
+}
+    return $reparaciones;
+
+}
+
+
+
+function getReparacion(PDO $con, int $id_veh_cliente,int $estado=0){
+	//devuelve todas las reparaciones de un vehiculo en concreto
+	//retorna un array $reparaciones[fila][columna]
+
+
+	if($estado==0){
+ 		   $stmt = $con->prepare("SELECT * FROM REPARACION WHERE ID_VEHICULO = ?");
+		}
+	else{
+ 		   $stmt = $con->prepare("SELECT * FROM REPARACION WHERE ID_VEHICULO = ? AND ID_EST_REP=?");
+
+		}
+    $id_vehiculo_array[0] = $id_veh_cliente;
+
+    if ($estado!=0) {
+    $id_vehiculo_array[1] = $estado;
+    	
+    }
+
+    $stmt ->execute($id_vehiculo_array);
+    $reparaciones = $stmt->fetchAll();
+    return $reparaciones;
+}
+
+
+
+
+function getEstados(PDO $con){
+    $stmt = $con->prepare("SELECT ID_EST_REP, ESTADO FROM ESTADO_REPARACION");
+    $stmt ->execute();
+    $estados = $stmt->fetchAll();
+    return $estados;
+}
+
+function getEstadoByID(PDO $con, int $id_REP){
+	
+
+	 $stmt = $con->prepare("SELECT ID_EST_REP FROM REPARACION where ID_REP = ?");
+	 $IDREPArray[0]=$id_REP;
+    $stmt ->execute($IDREPArray);
+    $IDestado = $stmt->fetchAll();
+    
+
+
+
+	 $stmt = $con->prepare("SELECT ESTADO FROM ESTADO_REPARACION where ID_EST_REP = ?");
+	 $estArray[0]=$IDestado[0][0];
+    $stmt ->execute($estArray);
+    $estados = $stmt->fetchAll();
+    return $estados;
+}
--- modelo/modeloVehiculo.php
+++ modelo/modeloVehiculo.php
@@ -1,1 +1,82 @@
 <?php
+
+
+function getVehiculosConcesionario($conex){
+
+		$stmt = $conex->prepare( 'select * from VEHICULO_CONCESIONARIO');
+		$stmt->execute();
+		$veh_conces = $stmt->fetchAll();
+
+		return $veh_conces;
+}
+
+
+function getVehConsFiltrado($conex,$precio,$marca,$modelo){
+		$precio = (float) $precio;
+		$stmt = $conex->prepare( 'select * from VEHICULO_CONCESIONARIO where PRECIO<=? AND MARCA=? AND MODELO=?');
+		$filtrosarray[0]=$precio;
+		$filtrosarray[1]=$marca;
+		$filtrosarray[2]=$modelo;
+
+		$stmt->execute($filtrosarray);
+
+		$veh_conces = $stmt->fetchAll();	
+
+		return $veh_conces;
+}
+
+function getMarcasVehiculosConcesionario($conex){
+
+		$stmt = $conex->prepare( 'select DISTINCT MARCA from VEHICULO_CONCESIONARIO');
+		$stmt->execute();
+		$mveh_conces = $stmt->fetchAll();
+
+		return $mveh_conces;
+}
+
+function getModelosVehiculosConcesionario($conex){
+
+		$stmt = $conex->prepare( 'select DISTINCT MODELO from VEHICULO_CONCESIONARIO');
+		$stmt->execute();
+		$mveh_conces = $stmt->fetchAll();
+
+		return $mveh_conces;
+}
+
+function getIDVehiculosCliente(PDO $con, int $ID_USUARIO){
+    $stmt = $con->prepare("SELECT ID_VEHICULO FROM VEHICULO_CLIENTE WHERE ID_USUARIO = ?");
+    $ID_USUARIOARRAY [0] = $ID_USUARIO;
+    $stmt ->execute($ID_USUARIOARRAY);
+    $id_veh_cliente = $stmt->fetchAll();
+    return $id_veh_cliente;
+}
+
+function getMarcayModeloVehCliente(PDO $con, int $ID_VEH){
+	$stmt = $con->prepare("SELECT MARCA,MODELO FROM VEHICULO WHERE ID_VEHICULO = ?");
+    $ID_VEHARRAY [0] = $ID_VEH;
+    $stmt ->execute($ID_VEHARRAY);
+    $veh_cliente = $stmt->fetchAll();
+    return $veh_cliente;
+}
+
+function getVehiculosCliente(PDO $con, array $id_veh_cliente){
+	//retorna marca y modelo de todos los vehiculos de un cliente
+    $cont = 0;
+    $vehiculos=[];
+    foreach ($id_veh_cliente as $id){
+        $vehiculos[$cont] = getVehiculo($con, $id[0]);
+        $cont++;
+    }
+
+    return $vehiculos;
+}
+
+
+function getVehiculo(PDO $con, int $id_veh_cliente){
+	//retorna modelo y marca pasandole un id veh
+    $stmt = $con->prepare("SELECT MODELO, MARCA FROM VEHICULO WHERE ID_VEHICULO = ?");
+    $id_vehiculo_array[0] = $id_veh_cliente;
+    $stmt ->execute($id_vehiculo_array);
+    $vehiculos = $stmt->fetchAll();
+    return $vehiculos;
+}
