402c72bb12031dd579574b22f8a23a5bbd23f357
27max <38960105+27max@users.noreply.github.com>
2018-06-01 21:31:44 +0200

    Add files via upload

--- modelo/modeloCliente.php
+++ modelo/modeloCliente.php
@@ -30,3 +30,35 @@ function addVehCliente(PDO $conex, $modelo,$marca,$id_usu,$anyo,$matricula){
 	 $stmt ->execute($arrayDV);
 
 }
+
+function updateVehCliente(PDO $conex, $modelo,$marca,$id_usu,$anyo,$matricula,$idVeh){
+	$stmt = $conex->prepare("UPDATE VEHICULO SET MODELO=?,FECHA_ANYO = TO_DATE(?,'yyyy/mm/dd'),MARCA=? WHERE ID_VEHICULO = ?");
+	$arrayDatos[0]=$modelo;
+	$arrayDatos[1]=$anyo;
+	$arrayDatos[2]=$marca;
+	$arrayDatos[3]=$idVeh;
+    $stmt ->execute($arrayDatos);
+
+  
+	
+	$stmt = $conex->prepare("UPDATE VEHICULO_CLIENTE SET MATRICULA=? WHERE ID_VEHICULO = ?  ");
+	
+	$arrayDV[0]=$matricula;
+	$arrayDV[1]=$idVeh;
+
+	
+
+	 $stmt ->execute($arrayDV);
+
+}
+
+function deleteVehCliente(PDO $conex, $idVeh){
+	$stmt = $conex->prepare("DELETE FROM VEHICULO_CLIENTE where ID_VEHICULO = ? ");
+	$arrayDV[0]=$idVeh;
+	 $stmt ->execute($arrayDV);
+
+	 	$stmt = $conex->prepare("DELETE FROM VEHICULO where ID_VEHICULO = ? ");
+	$arrayDV[0]=$idVeh;
+	 $stmt ->execute($arrayDV);
+
+}
