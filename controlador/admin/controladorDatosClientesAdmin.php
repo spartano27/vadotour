08396efcc4d2d6992baebc565df1b766e00ede44
27max <38960105+27max@users.noreply.github.com>
2018-06-01 18:39:26 +0200

    Add files via upload

--- controlador/admin/controladorDatosClientesAdmin.php
+++ controlador/admin/controladorDatosClientesAdmin.php
@@ -1,1 +1,19 @@
 <?php
+
+include_once "core/db.php";
+$conex= crearConexionBD();
+include_once 'modelo/modeloCliente.php';
+include_once 'modelo/modeloVehiculo.php';
+
+$clientes = getClientes($conex);
+
+if(isset($_REQUEST['add'])){
+	$vehiculosCliente= getVehiculosCliente($conex,getIDVehiculosCliente($conex,$_REQUEST['idUsuario']));
+
+}
+
+
+if (isset($_REQUEST['addVeh'])){
+	addVehCliente($conex, $_REQUEST['modelo'],$_REQUEST['marca'],$_REQUEST['idUsuario'],$_REQUEST['anyo'],$_REQUEST['matricula']);
+
+}
