65fa97c058b26ceb8d3d0f3886bd85b25115e95e
spartano27 <39529663+spartano27@users.noreply.github.com>
2018-06-02 05:58:17 +0200

    Update controladorInformacionVehiculo.php

--- controlador/controladorInformacionVehiculo.php
+++ controlador/controladorInformacionVehiculo.php
@@ -1,1 +1,23 @@
 <?php
+
+include_once 'modelo/modeloVehiculo.php';
+include_once "core/db.php";
+$con= crearConexionBD();
+
+function getVehConsFiltradoPorID($con,$ID_VEHICULO){
+		$stmt = $con->prepare( 'select * from VEHICULO_CONCESIONARIO where $ID_VEHICULO');
+		$filtrosarray[0]=$ID_VEHICULO;
+
+		$stmt->execute($filtrosarray);
+
+		$veh_conces = $stmt->fetchAll();	
+
+	return $veh_conces;}
+
+//se pulsa el boton Pedir cita
+if(isset($_REQUEST["pedirCita"])){
+    
+    header("Location: vistaPedirCita.php");
+    
+    
+}
